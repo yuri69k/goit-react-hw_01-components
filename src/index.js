e38554ee765ba14c3811fcc56350cf32ec89af26
yuri.karmaz <106161829+yuri69k@users.noreply.github.com>
2022-12-18 20:48:14 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,16 +1,15 @@
 import { default as React } from 'react';
-import { default as ReactDOM } from 'react-dom';
+import { default as ReactDOM } from 'react-dom/client';
 import { ThemeProvider } from 'styled-components';
 
 import './index.css';
 import { App } from './components/App/App';
 import { theme } from './constants';
 
-ReactDOM.render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <App />
     </ThemeProvider>
-  </React.StrictMode>,
-  document.getElementById('root')
+  </React.StrictMode>
 );
