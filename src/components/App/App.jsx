e9302739be318fd0c8d37dc15739fe139126a075
yuri.karmaz <106161829+yuri69k@users.noreply.github.com>
2022-12-19 13:09:08 +0200

    test build

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,9 +1,9 @@
 // import { Profile, Statistics, FriendList, TransactionHistory } from '../';
 
-import { Profile } from '../../Profile/Profile';
-import { Statistics } from '../../Statistics/Statistics';
-import { FriendList } from '../../FriendList/FriendList';
-import { TransactionHistory } from '../../TransactionHistory/TransactionHistory';
+import { Profile } from '../Profile/Profile';
+import { Statistics } from '../Statistics/Statistics';
+import { FriendList } from '../FriendList/FriendList';
+import { TransactionHistory } from '../TransactionHistory/TransactionHistory';
 import { user, data, friends, transactions } from '../../data';
 import { Container } from './App.styled';
 
