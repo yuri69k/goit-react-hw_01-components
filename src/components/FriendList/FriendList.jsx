2127c7733ed29252c2197fce55f860adf2763db1
yuri.karmaz <106161829+yuri69k@users.noreply.github.com>
2022-12-20 14:52:25 +0200

    correct review

--- src/components/FriendList/FriendList.jsx
+++ src/components/FriendList/FriendList.jsx
@@ -3,7 +3,24 @@ import { FriendListStyled } from './FriendList.styled';
 import FriendListItem from './FriendListItem';
 
 export const FriendList = ({ friends }) => {
-  return <FriendListStyled> {friends.map(FriendListItem)} </FriendListStyled>;
+  return <FriendListStyled>   {friends.map(({avatar, name, isOnline, id}) => (
+                <FriendListItem
+                    key={id}
+                    avatar={avatar}
+                    name={name}
+                    isOnline={isOnline}
+                    id={id}
+                />
+            ))} </FriendListStyled>;
 };
 
-FriendList.propTypes = { friends: PropTypes.arrayOf(PropTypes.object) };
+FriendList.propTypes = {
+  friends: PropTypes.arrayOf(
+    PropTypes.exact({
+      avatar: PropTypes.string.isRequired,
+      name: PropTypes.string.isRequired,
+      isOnline: PropTypes.bool.isRequired,
+      id: PropTypes.number.isRequired,
+    })
+  ),
+};
